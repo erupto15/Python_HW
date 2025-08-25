4575ea46419db64036a9c437d2c06853cc8ac533
Vlad Shevchenko <shevchenko.vlad98@mail.ru>
2025-08-25 20:27:16 +0300

    Lesson_1

--- Lesson_1/Task_1.py
+++ Lesson_1/Task_1.py
@@ -2,23 +2,6 @@ my_name = ('Владислав')
 print(my_name)
 
 
-#pet_name = input("Кеша")
-#print("Кеша" + pet_name)
 
-#def print_python():
-    #print ("Учу Python!")
-
-#print_python()
-
-#def print_letter(lef):
-   # print(lef, end='')
-
-#print_letter('С')
-#print_letter('т')
-#print_letter('у')
-#print_letter('д')
-#print_letter('е')
-#print_letter('н')
-#print_letter('т')
 
 
